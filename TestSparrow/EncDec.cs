6f8f82a83685b4fee32d00bccb196546eee323a7
Dmitry Marchuk <dmitry.marchuk@atola.com>
2014-01-29 15:41:18 +0200

    Initial commit.

---
+++ TestSparrow/EncDec.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSparrow
+{
+    class EncDec : ICrypto
+    {
+        static string Encode(string text)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (char item in text.ToCharArray())
+            {
+                foreach (byte b in BitConverter.GetBytes(item).Reverse())
+                {
+                    builder.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        static string Decode(string binaryString)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < binaryString.Length; i += sizeof(char) * 8)
+            {
+                string str = binaryString.Substring(i, sizeof(char) * 8);
+                builder.Append((char)Convert.ToInt16(str, 2));
+            }
+
+            return builder.ToString();
+        }
+
+        string ICrypto.Encode(string s)
+        {
+            return EncDec.Encode(s);
+        }
+
+        string ICrypto.Decode(string s)
+        {
+            return EncDec.Decode(s);
+        }
+    }
+}
---
+++ TestSparrow/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestSparrow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private ICrypto __EncoderDecoder = new EncDec();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            App.Current.Activated += Current_Activated;
+        }
+
+        private void Current_Activated(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(LeftText))
+                LeftText = Clipboard.GetText();
+            if (String.IsNullOrWhiteSpace(RightText))
+                RightText = Clipboard.GetText();
+        }
+
+        private string __LeftText;
+
+        public string LeftText
+        {
+            get { return __LeftText; }
+            set { __LeftText = value; OnPropertyChanged("LeftText"); }
+        }
+
+        private string __RightText;
+
+        public string RightText
+        {
+            get { return __RightText; }
+            set { __RightText = value; OnPropertyChanged("RightText"); }
+        }
+
+        private void EncodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            LeftText = Clipboard.GetText();
+            try
+            {
+                LeftText = __EncoderDecoder.Encode(LeftText);
+            }
+            catch { }
+        }
+
+        private void DecodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                RightText = __EncoderDecoder.Decode(RightText);
+            }
+            catch { }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        // Create the OnPropertyChanged method to raise the event 
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private void TextBox_PreviewMouseUp(object sender, EventArgs e)
+        {
+            TextBox b = sender as TextBox;
+            if (b != null)
+            {
+                b.SelectAll();
+            }
+        }
+    }
+}
